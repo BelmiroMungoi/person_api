4d09268ad84b1374b2f267db66cbbd1118875e86
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-23 20:45:43 +0200

    configuracoes basicas para a app

--- src/main/java/com/bbm/person/api/PersonApiApplication.java
+++ src/main/java/com/bbm/person/api/PersonApiApplication.java
@@ -1,9 +1,23 @@
 package com.bbm.person.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.bbm.person.api.model")
+@ComponentScan(basePackages = "com.*")
+@EnableJpaRepositories(basePackages = "com.bbm.person.api.repository")
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class PersonApiApplication {
 
 	public static void main(String[] args) {
