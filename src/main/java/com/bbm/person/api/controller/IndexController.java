85a8780e94999612b07f3a7e3ca43fb519f5d2c3
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-24 22:21:36 +0200

    criando o metodo para salvar usuario

--- src/main/java/com/bbm/person/api/controller/IndexController.java
+++ src/main/java/com/bbm/person/api/controller/IndexController.java
@@ -6,8 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbm.person.api.model.Usuario;
@@ -19,20 +20,28 @@ public class IndexController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> saveUser(@RequestBody Usuario usuario) {
+
+		Usuario user = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+
 	@GetMapping(value = "/", produces = "application/json")
 	public ResponseEntity<List<Usuario>> findAll() {
-		
+
 		List<Usuario> usuarios = usuarioRepository.findAll();
-		
+
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario> findById(@PathVariable("id") Long id){
-		
+
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> findById(@PathVariable("id") Long id) {
+
 		Usuario usuario = usuarioRepository.findById(id).get();
-		
+
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
 }
