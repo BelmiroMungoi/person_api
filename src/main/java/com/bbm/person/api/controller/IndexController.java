b5d2003e80f62be8f7d22d32418decc5a4d9e13f
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-24 23:09:19 +0200

    adicionando o metodo para deletar

--- src/main/java/com/bbm/person/api/controller/IndexController.java
+++ src/main/java/com/bbm/person/api/controller/IndexController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,7 +48,7 @@ public class IndexController {
 
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
-	
+
 	@PutMapping(value = "/", produces = "application/json")
 	@ResponseBody
 	public ResponseEntity<?> updateUser(@RequestBody Usuario usuario) {
@@ -61,6 +62,15 @@ public class IndexController {
 
 			return new ResponseEntity<Usuario>(user, HttpStatus.OK);
 		}
+
+	}
+
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+
+		usuarioRepository.deleteById(id);
+
+		return new ResponseEntity<String>("Usuário Deletado com Sucesso", HttpStatus.OK);
 
 	}
 }
