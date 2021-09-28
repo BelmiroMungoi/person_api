b70825d9085bebcfb3d79b0e666468746b8ef29c
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-28 13:22:35 +0200

    criando a query para a busca do usuario

--- src/main/java/com/bbm/person/api/repository/UsuarioRepository.java
+++ src/main/java/com/bbm/person/api/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.bbm.person.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bbm.person.api.model.Usuario;
@@ -8,4 +9,6 @@ import com.bbm.person.api.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	@Query("select u from Usuario u where u.userName = ?1")
+	Usuario findByUserName(String userName);
 }
