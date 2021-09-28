0a382d85d278710c41182c172acbeac813511fb7
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-28 10:39:13 +0200

    liberado o acesso de toda api para requisicoes de outros servers

--- src/main/java/com/bbm/person/api/PersonApiApplication.java
+++ src/main/java/com/bbm/person/api/PersonApiApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.bbm.person.api.model")
@@ -18,10 +20,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class PersonApiApplication {
+public class PersonApiApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApiApplication.class, args);
+	}
+	
+	//Mapeamento global
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//Libera o acesso aos End-Points da API
+		registry.addMapping("/**")
+		.allowedMethods("*")// libera quais metodos https(GET, POST, etc) podem ser acessados
+		.allowedOrigins("*");// libera o acesso para requisicoes de outros servers
 	}
 
 }
