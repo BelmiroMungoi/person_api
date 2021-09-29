ea70caf52613b9d0078d4609a6d90b511ea506a8
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-29 19:52:01 +0200

    classe que filtra todas as requisicoes

---
+++ src/main/java/com/bbm/person/api/security/JwtApiAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.bbm.person.api.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+//Filter onde todas as requisicoes serao capturadas para autenticacao
+public class JwtApiAuthenticationFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		//Estabelece a autenticacao para a requisacao
+		Authentication authentication = new JwtAuthenticationService()
+				.getAuthentication((HttpServletRequest) request);
+		
+		//Coloca o processo de autenticacao no spring security
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	
+		//Continua o processo
+		chain.doFilter(request, response);
+	}
+
+}
