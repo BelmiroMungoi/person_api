6765e62e960f57cad1941746b8e738031946e55c
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-01 12:17:38 +0200

    ativando o cache para perfomace

--- src/main/java/com/bbm/person/api/PersonApiApplication.java
+++ src/main/java/com/bbm/person/api/PersonApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
+@EnableCaching
 public class PersonApiApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
