a408d45f2134e383ce294fa25e6dc9eb55d0da0a
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-01 18:44:38 +0200

    criando a query para a atualizacao do token

--- src/main/java/com/bbm/person/api/repository/UsuarioRepository.java
+++ src/main/java/com/bbm/person/api/repository/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package com.bbm.person.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bbm.person.api.model.Usuario;
 
@@ -11,4 +13,9 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	@Query("select u from Usuario u where u.userName = ?1")
 	Usuario findByUserName(String userName);
+	
+	@Modifying
+	@Transactional
+	@Query(nativeQuery = true, value = "update usuario set token = ?1 where user_name = ?2")
+	void updateTokenUser(String token, String userName);
 }
