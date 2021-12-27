1bad2ca2ff5ddbb205fb7005548526330e9bed26
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-12-27 09:29:33 +0200

    enviando o email com a nova senha

--- src/main/java/com/bbm/person/api/controller/RecuperaController.java
+++ src/main/java/com/bbm/person/api/controller/RecuperaController.java
@@ -1,5 +1,8 @@
 package com.bbm.person.api.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bbm.person.api.exception.ErrorObject;
 import com.bbm.person.api.model.Usuario;
 import com.bbm.person.api.repository.UsuarioRepository;
+import com.bbm.person.api.service.SendMailService;
 
 @RestController
 @RequestMapping(value = "/recuperar")
@@ -19,17 +23,30 @@ public class RecuperaController {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	@Autowired
+	private SendMailService mailService;
+	
 	@PostMapping(value = "/")
-	public ResponseEntity<ErrorObject> recuperar(@RequestBody Usuario login) {
+	public ResponseEntity<ErrorObject> recuperar(@RequestBody Usuario login) throws Exception {
 		
 		ErrorObject error = new ErrorObject();
 		
 		Usuario user = repository.findByUserName(login.getUsername());
 		
-		if (user == null) {
+		if (user == null) {			
 			error.setCode("404");
 			error.setError("Usuário não encontrado");
 		} else {
+			
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			String senhaNova = dateFormat.format(Calendar.getInstance().getTime());
+			
+			repository.updateSenha(senhaNova, user.getId());
+			
+			mailService.sendMail("Recuperacão de Senha",
+					user.getUserName(),
+					"Sua senha nova é: " + senhaNova);
+			
 			error.setCode("200");
 			error.setError("Acesso enviado para o seu e-mail!");
 		}
