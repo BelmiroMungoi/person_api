bb4cd573e47e71371c54291386706640989a76e2
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-17 16:20:49 +0200

    classe que iraa gerar o relatorio

---
+++ src/main/java/com/bbm/person/api/service/ReportService.java
@@ -0,0 +1,40 @@
+package com.bbm.person.api.service;
+
+import java.io.File;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+@Service
+public class ReportService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public byte[] gerarRelatorio(String nomeRelatorio, ServletContext context) throws Exception {
+		
+		//Obter conexao com a base de dados
+		Connection connection = jdbcTemplate.getDataSource().getConnection();
+		
+		//Carregar o Caminho do file jasper
+		String caminho = context.getRealPath("reports") + File.separator + nomeRelatorio + ".jasper";
+		
+		//Gerar relatorio com dados e impressao
+		JasperPrint print = JasperFillManager.fillReport(caminho, new HashMap<>(), connection);
+		
+		//Exporta para byte o pdf para realizar o download
+		return JasperExportManager.exportReportToPdf(print);
+	}
+}
