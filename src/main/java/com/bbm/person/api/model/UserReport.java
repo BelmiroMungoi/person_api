a21b71da90f35cb119e0badf3459cc6926d4a551
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-18 20:51:19 +0200

    model para os parametros do relatorio

---
+++ src/main/java/com/bbm/person/api/model/UserReport.java
@@ -0,0 +1,24 @@
+package com.bbm.person.api.model;
+
+public class UserReport {
+
+	private String dataInicial;
+	private String dataFinal;
+
+	public String getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(String dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public String getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(String dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+
+}
