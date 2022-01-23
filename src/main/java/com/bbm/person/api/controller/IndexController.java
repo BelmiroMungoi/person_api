fea537b56c5eca0dce7e613abe3594a7571a2c05
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-23 18:51:59 +0200

    modificando o sql

--- src/main/java/com/bbm/person/api/controller/IndexController.java
+++ src/main/java/com/bbm/person/api/controller/IndexController.java
@@ -217,7 +217,7 @@ public class IndexController {
 		UserChart userChart = new UserChart();
 
 		List<String> result = jdbcTemplate.queryForList(
-				"select array_agg( '''' || full_name || '''') from usuario where salario > 0 and full_name <> '' union all select cast(array_agg(salario) as character varying[]) from usuario where salario > 0 and full_name <> ''",
+				"select array_agg(full_name) from usuario where salario > 0 and full_name <> '' union all select cast(array_agg(salario) as character varying[]) from usuario where salario > 0 and full_name <> ''",
 				String.class);
 		
 		if (!result.isEmpty()) {
