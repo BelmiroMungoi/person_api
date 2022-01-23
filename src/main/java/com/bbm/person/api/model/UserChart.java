c9df7d7ee7ed812b850f2e1694651ef8e685850b
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-23 12:20:33 +0200

    adicionando a classe com os atributos do grafico

---
+++ src/main/java/com/bbm/person/api/model/UserChart.java
@@ -0,0 +1,24 @@
+package com.bbm.person.api.model;
+
+public class UserChart {
+
+	private String fullName;
+	private String salario;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getSalario() {
+		return salario;
+	}
+
+	public void setSalario(String salario) {
+		this.salario = salario;
+	}
+
+}
